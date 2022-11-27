a1704d16bcc4a9261e3aa25208b5dff63232ea26
ffresanto <ffresanto@gmail.com>
2022-11-27 18:02:40 -0300

    Create DB

---
+++ becominglegend_api/src/BecomingLegend.API/Commands.md
@@ -0,0 +1,13 @@
+## Project Commands
+
+`dotnet run` 
+`dotnet build` 
+`dotnet watch run` - Run and open swagger in navegation
+
+## Execute Migrations
+
+`dotnet ef migrations add initial -o Data/Migrations`
+
+## Create DB
+
+`dotnet ef database update`
--- becominglegend_api/src/BecomingLegend.API/Program.cs
+++ becominglegend_api/src/BecomingLegend.API/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddDbContext<DataContext>(
-    options => options.UseSqlite(builder.Configuration.GetConnectionString(""))
+    options => options.UseSqlite(builder.Configuration.GetConnectionString("Default"))
 );
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
